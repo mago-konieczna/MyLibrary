73f163e4966a144ec21847c9029dbca91230e689
Malgorzata Konieczna <gola434@wp.pl>
2023-01-05 13:19:26 +0100

    creating a new Library DbContext class and information about the tables that will be in the database

---
+++ MyLibrary/Models/LibraryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace MyLibrary.Models
+{
+    public class LibraryDbContext : DbContext   
+    {
+        public DbSet<Book> Books { get; set; }  
+        public DbSet<Author> Authors { get; set; }  
+        public DbSet<Publisher> Publishers { get; set; }  
+    }
+}
