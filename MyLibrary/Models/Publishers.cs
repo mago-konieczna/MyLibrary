cf9a578dce05ae4c578b5d42205ee0de2a732498
Malgorzata Konieczna <gola434@wp.pl>
2023-02-18 17:51:54 +0100

    Changing

--- MyLibrary/Models/Publishers.cs
+++ MyLibrary/Models/Publishers.cs
@@ -1,14 +1,13 @@
-﻿namespace MyLibrary.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MyLibrary.Models
 {
     public class Publishers
     {
         public int Id { get; set; } 
         public string PublisherName { get; set; }
         /* may add more properties in the future for example: Country, Adress, ContactEmail, ContactNumber */
-
-
-      
-
+        [JsonIgnore]
         public List<Book> Books { get; set; }
 
 
